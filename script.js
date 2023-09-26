760e1b604e4c429462fddd3f4d37846c09742b2c
Austin <samichpower@yahoo.com>
2023-09-26 07:11:50 -0500

    Append the prompt to the HTML page

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const promptContainer = document.getElementById('prompt-container');
 
+const feelings = ['Sinister', 'Happiness', 'Sadness', 'Anger', 'Love', 'Fear', 'Hope', 'Loneliness', 'Excitement', 'Calmness', 'Nostalgia', 'Confidence', 'Mystery', 'Surprise', 'Regret', 'Peace', 'Passion', 'Longing', 'Anxiety', 'Awe', 'Desire', 'Euphoria', 'Grief', 'Curiosity', 'Optimism', 'Discontent', 'Gratitude', 'Pride', 'Tension'];
 const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 const scales = ['Major (Ionian) Scale', 'Natural Minor (Aeolian) Scale', 'Harmonic Minor Scale', 'Melodic Minor Scale', 'Dorian Mode', 'Phrygian Mode', 'Lydian Mode', 'Mixolydian Mode', 'Locrian Mode', 'Pentatonic Major Scale', 'Pentatonic Minor Scale', 'Blues Scale', 'Hungarian Minor Scale', 'Phrygian Dominant Scale', 'Whole Tone Scale'];
-const feelings = ['Sinister', 'Happiness', 'Sadness', 'Anger', 'Love', 'Fear', 'Hope', 'Loneliness', 'Excitement', 'Calmness', 'Nostalgia', 'Confidence', 'Mystery', 'Surprise', 'Regret', 'Peace', 'Passion', 'Longing', 'Anxiety', 'Awe', 'Desire', 'Euphoria', 'Grief', 'Curiosity', 'Optimism', 'Discontent', 'Gratitude', 'Pride', 'Tension'];
 
-function getRandomNum(arr) {
+function getRandomItem(arr) {
   return Math.floor(Math.random() * arr.length);
 }
 
-const prompt = `Write a song that sounds like ${feelings[getRandomNum(feelings)]} in ${keys[getRandomNum(keys)]} with the ${scales[getRandomNum(scales)]}`
+function appendPrompt() {
+  const feeling = feelings[getRandomItem(feelings)];
+  const key = keys[getRandomItem(keys)];
+  const scale = scales[getRandomItem(scales)];
 
-console.log(prompt);
+  promptContainer.innerHTML = `Write a song that sounds like <span class="feeling item">${feeling}</span> in <span class="key item">${key}</span> with the <span class="scale item">${scale}</span>`;
+}
+
+appendPrompt();
