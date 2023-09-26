c65be3a4977a3a1f6991e8d583e486bcb2902725
Austin <samichpower@yahoo.com>
2023-09-26 18:19:07 -0500

    Minor adjustments for readability

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 const scales = ['Major (Ionian) Scale', 'Natural Minor (Aeolian) Scale', 'Harmonic Minor Scale', 'Melodic Minor Scale', 'Dorian Mode', 'Phrygian Mode', 'Lydian Mode', 'Mixolydian Mode', 'Locrian Mode', 'Pentatonic Major Scale', 'Pentatonic Minor Scale', 'Blues Scale', 'Hungarian Minor Scale', 'Phrygian Dominant Scale', 'Whole Tone Scale'];
 
 function appendPrompt() {
-  function getRandomItem(arr) {
-    return Math.floor(Math.random() * arr.length);
-  }
-
   const feeling = document.getElementById('feeling');
   const key = document.getElementById('key');
   const scale = document.getElementById('scale');
+  
+  function getRandomItem(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
 
   feeling.textContent = feelings[getRandomItem(feelings)];
   key.textContent = keys[getRandomItem(keys)];
