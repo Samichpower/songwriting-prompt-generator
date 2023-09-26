12546e6c32e62771802f25ce2a1a8a74116fabc1
Austin <samichpower@yahoo.com>
2023-09-26 06:55:29 -0500

    Fix typo in prompt

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const promptContainer = document.getElementById('prompt-container');
 
 const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 const scales = ['Major (Ionian) Scale', 'Natural Minor (Aeolian) Scale', 'Harmonic Minor Scale', 'Melodic Minor Scale', 'Dorian Mode', 'Phrygian Mode', 'Lydian Mode', 'Mixolydian Mode', 'Locrian Mode', 'Pentatonic Major Scale', 'Pentatonic Minor Scale', 'Blues Scale', 'Hungarian Minor Scale', 'Phrygian Dominant Scale', 'Whole Tone Scale'];
-const feelings = ['Sinister', 'Happiness', 'Sadness', 'Anger', 'Love', 'Fear', 'Hope', 'Loneliness', 'Excitement', 'Calmness', 'Nostalgia', 'Confidence', 'Mystery', 'Surprise', 'Regret', 'Peace', 'Passion', 'Longing', 'Anxiety', 'Awe', 'Desire', 'Euphoria', 'Grief', 'Curiosity', 'Optimism', 'Discontent', 'Gratitude', 'Pride', 'Tension'];
+const feelings = ['Sinister', 'Happy', 'Sad', 'Angry', 'Love', 'Fear', 'Hope', 'Loneliness', 'Excitement', 'Calmness', 'Nostalgia', 'Confidence', 'Mystery', 'Surprise', 'Regret', 'Peace', 'Passion', 'Longing', 'Anxiety', 'Awe', 'Desire', 'Euphoria', 'Grief', 'Curiosity', 'Optimism', 'Discontent', 'Gratitude', 'Pride', 'Tension'];
 
 function getRandomNum(arr) {
   return Math.floor(Math.random() * arr.length);
 }
 
-const prompt = `Write a song that sounds ${feelings[getRandomNum(feelings)]} in ${keys[getRandomNum[keys]]} with the ${scales[getRandomNum[scales]]}`
+const prompt = `Write a song that sounds ${feelings[getRandomNum(feelings)]} in ${keys[getRandomNum(keys)]} with the ${scales[getRandomNum(scales)]}`
 
 console.log(prompt);
